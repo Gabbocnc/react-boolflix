4dda166608ed429dac7b80d09f30cd3006da8149
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-28 14:25:50 +0100

    add Flag

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,32 +1,51 @@
 import { useContext } from 'react';
 import GlobalContext from '../context/GlobalContext';
-/* import { IT, US, FR, ES, JP, GB, } from 'country-flag-icons/react/3x2' */
+import { IT, US, FR, ES, JP, GB, DE, CN, KR, RU } from 'country-flag-icons/react/3x2'
 
 export default function MovieList() {
-
-
     const { filteredMovies } = useContext(GlobalContext);
-    /*   const languageFlag = {
-          'it': IT,
-          'en': US,
-          'fr': FR,
-          'es': ES,
-          'ja': JP,
-          'gb': GB,
-      }; */
+
+    /* Bandiere per le lingue */
+    const languageFlag = {
+        'it': IT,
+        'en': US,
+        'fr': FR,
+        'es': ES,
+        'ja': JP,
+        'gb': GB,
+        'de': DE,
+        'cn': CN,
+        'ko': KR,
+        'ru': RU
+    };
 
     return (
         <div>
             <h2>Film Trovati</h2>
             <ul>
-                {filteredMovies.map((movie) => (
-                    <li key={movie.id}>
-                        <h3>{movie.title}</h3>
-                        <p>{movie.original_title}</p>
-                        <p>Lingua : {movie.original_language}</p>
-                        <p>Voto : {movie.vote_average}</p>
-                    </li>
-                ))}
+                {filteredMovies.map((movie) => {
+
+                    const Flag = languageFlag[movie.original_language.toLowerCase()];
+
+                    return (
+                        <li key={movie.id}>
+                            <h3>{movie.title}</h3>
+                            <p>{movie.original_title}</p>
+                            <div style={{}}>
+                                {Flag ? (
+                                    <Flag
+                                        width={30}
+                                        height={20}
+                                        language={movie.original_language}
+                                    />
+                                ) : (
+                                    <span>{movie.original_language}</span>
+                                )}
+                                <p>Voto: {movie.vote_average}</p>
+                            </div>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     );
